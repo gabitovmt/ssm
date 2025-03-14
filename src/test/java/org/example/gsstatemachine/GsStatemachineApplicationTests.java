32415c2da5e10b51bc8d7b208616f8b65f0d15bf
Marat Gabitov <gabitovmt@yandex.ru>
2025-03-14 18:53:59 +0500

    init

---
+++ src/test/java/org/example/gsstatemachine/GsStatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.gsstatemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GsStatemachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
